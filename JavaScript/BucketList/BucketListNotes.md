ec1eeff3184632b699cc311f3ce1b7e298bba13e
Joseph Minton <joseph.minton@usmc.mil>
2025-08-26 14:37:51 -0500

    Worked on COMP 2 PDFs

---
+++ JavaScript/BucketList/BucketListNotes.md
@@ -0,0 +1,18 @@
+BucketList App
+
+HTML = Form
+1 input = description of a bucket list item
+button = submit
+click submit
+create a new task
+display it at the end of the current list
+toggle complete task = crUd = update
+
+<ul>
+    <li>hello</li>
+    <li>this is a bucket list</li>
+    <li>use the arrow keys to move up or down</li>
+    <li>yada yada</li>
+    <li>goodbye</li>
+</ul>
+
---
+++ JavaScript/DataTypeDemo/COMP 2 PDFS.js
@@ -0,0 +1,73 @@
+const prompt = require('prompt-sync')({ sigint: true })
+
+// ACTIVITY TWO.1
+// let result = prompt(`What is your name`)
+// Number(String)
+// console.log(result);
+
+// ACTIVITY TWO.2
+// let userString = String(prompt(`Enter a String`))
+// let userNumber1 = parseInt(prompt(`Enter a Start index for cutting`))
+// let userNumber2 = parseInt(prompt(`Enter an End index for cutting`))
+// let newString = userString.substring(userNumber1,userNumber2);
+// console.log(newString)
+
+// ACTIVITY TWO.3
+// let userSentence = String(prompt(`Enter in a sentence`))
+// let word = String(prompt(`Enter a word from that sentence`))
+// let index = userSentence.indexOf(word)
+// console.log(index)
+
+//ACTIVITY THREE
+// let number = Number(prompt(`Enter a number`))
+// if(number % 2 === 0) {console.log(`number is even`)}
+// else if(number % 2 === 1) { console.log(`number is odd`)}
+
+//ACTIVITY FOUR
+// let fruits = ['apple', 'banana', 'orange', 'mango', 'grape'];
+// let userFruit = String(prompt('Enter a fruit name:'))
+// console.log(
+//     fruits.includes(userFruit.toLowerCase())
+//         ? `${userFruit} is in the fruit array!`
+//         : `${userFruit} is not in the fruit array.`
+// );
+
+//ACTIVITY FIVE
+// let students = ["dOnaLD", "aLICE", "BoB","jOnAtHaN","CoDy"];
+// students.push("joe");
+// console.log(students);
+// console.log(students.includes("joe"))
+
+//ACTIVITY SIX
+// let person = {
+//     name: "Walter White",
+//     age: 50,
+//     family: ["Skylar", "Flynn", "Holly"],
+//     city: "Albuquerque",
+//     smart: true,
+//     associates: {
+//         friend: "Jesse",
+//         enemy: "Gus"
+//     },
+//     hobbies: "Cooking"
+// };
+//
+// console.log(person.age);
+// console.log(person.associates.friend);
+// console.log(person.associates.enemy);
+// console.log(person.family[2]);
+
+//ACTIVITY SEVEN
+// function calculateAreaOfRectangle(width, height) {
+//     let area = width * height
+//     return (console.log(area))
+// }
+// calculateAreaOfRectangle(5,5);
+// const calculateAreaOfRectangle = (width, height) => {
+//     const area = width * height;
+//     console.log(area);
+//     return area;
+// };
+// calculateAreaOfRectangle(5, 5);
+
+
