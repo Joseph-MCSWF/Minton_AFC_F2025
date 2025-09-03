2f65b2ec421aa719c3f2bf1096ed6fe65ab027a4
Joseph Minton <joseph.minton@usmc.mil>
2025-09-03 09:09:26 -0500

    Comp 3 Lab One Submissions

---
+++ JavaScript/Functions1/script.js
@@ -0,0 +1,44 @@
+// declaration and assignment
+// let squareFunction = (num) => {return console.log(`the square of ${num} is: ` + num * num)}
+//invocation
+// squareFunction(5)
+//declaration and assignment with 2 named variables
+// let areaOfRectangle = (height, width) => {return console.log(`the area of the square or rectangle is: ` + height * width)}
+// areaOfRectangle(5, 5)
+
+// person = {
+//     firstName: "Joseph",
+//     lastName: "Minton",
+//     fullName: function(){
+//         console.log(this.firstName + " " + this.lastName);
+//     }
+// }
+// person.fullName();
+// getFullName = (person ) => {return console.log("The persons name is: " + person.firstName + " " + person.lastName)};
+// function getFullName2(person) {return console.log("The persons name is: " + person.firstName + " " + person.lastName)}
+// getFullName(person)
+// getFullName2(person)
+
+//
+// let array1 = ["apple", "banana", "apple","orange","apricot","orange"];
+// let array2 = [1,2,3,4,5,6,7,8,9,10];
+// let array3 = []
+// sortArray = (array) => {
+//     if(array.length === 0){
+//         return "array is empty";
+//     }
+//     else if(typeof array[0] === "string"){
+//         return array.sort((a, b) => {
+//             if (a < b) return -1;
+//             if (a > b) return 1;
+//             return 0;
+//         });
+//     }
+//     else if(typeof array[0] === "number"){
+//         return array.sort((a,b) => a-b);
+//     }
+// }
+// console.log(sortArray(array1));
+// console.log(sortArray(array2));
+// console.log(sortArray(array3));
+
