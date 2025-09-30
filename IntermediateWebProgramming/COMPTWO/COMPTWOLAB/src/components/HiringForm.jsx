8fd6c0c96562468c2a4ce7f074f6c1a3ba2e097c
Joseph Minton <joseph.minton@usmc.mil>
2025-09-30 13:20:31 -0500

    Work

--- IntermediateWebProgramming/COMPTWO/COMPTWOLAB/src/components/HiringForm.jsx
+++ IntermediateWebProgramming/COMPTWO/COMPTWOLAB/src/components/HiringForm.jsx
@@ -4,25 +4,50 @@ import "../styles/Hiring.css";
 export default function HiringForm() {
     return(
         <div className="d-flex justify-content-center pt-5">
-            <Form className="hiring-form w-100" style={{ maxWidth: 900 }}>
+            <Form className="hiring-form w-100" style={{ maxWidth: 1200 }}>
+                {/*ROW ONE*/}
                 <Row className="g-3 align-items-center">
-                    <Col>
-                        <Label htmlFor="firstName" className="col-form-label mb-0">First Name</Label>
+                    <Col sm="12" md="4">
+                        <FormGroup>
+                            <Label htmlFor="firstName" className="col-form-label mb-0">First Name</Label>
+                            <Input id="firstName" type="text" placeholder="First Name" />
+                        </FormGroup>
                     </Col>
-                    <Col>
-                        <Input id="firstName" type="text" placeholder="First Name" />
+                    <Col sm="12" md="4">
+                        <FormGroup>
+                            <Label htmlFor="lastName" className="col-form-label mb-0">Last Name</Label>
+                            <Input id="lastName" type="text" placeholder="Last Name" />
+                        </FormGroup>
                     </Col>
-                    <Col>
-                        <Label htmlFor="lastName" className="col-form-label mb-0">Last Name</Label>
+                    <Col sm="12" md="4">
+                        <FormGroup>
+                            <Label htmlFor="phoneNumber" className="col-form-label mb-0">Phone Number</Label>
+                            <Input id="phoneNumber" type="text" placeholder="Phone Number" />
+                        </FormGroup>
                     </Col>
-                    <Col>
-                        <Input id="lastName" type="text" placeholder="Last Name" />
+                </Row>
+                {/*ROW TWO*/}
+                <Row className="g-3 align-items-center">
+                    <Col sm="12" md="4">
+                        <FormGroup>
+                            <Label htmlFor="firstName" className="col-form-label mb-0">Address</Label>
+                            <Input id="firstName" type="text" placeholder="Address" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="4">
+                        <FormGroup>
+                            <Label htmlFor="lastName" className="col-form-label mb-0">City</Label>
+                            <Input id="lastName" type="text" placeholder="City" />
+                        </FormGroup>
+                    </Col>
+                    <Col sm="12" md="4">
+                        <FormGroup>
+                            <Label htmlFor="phoneNumber" className="col-form-label mb-0">State</Label>
+                            <Input id="phoneNumber" type="text" placeholder="State" />
+                        </FormGroup>
                     </Col>
                 </Row>
             </Form>
         </div>
-
-
     )
-
 }
